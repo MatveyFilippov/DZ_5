83b46cd5c00124821e4b34647abae45f14e5ee85
MatveyFilippov <mdfilippov_2@edu.hse.ru>
2022-10-20 17:43:05 +0300

    4 commit

--- 1.py
+++ 1.py
@@ -1,29 +1,32 @@
 print("Здесь можно первести число из десятичной системы счисления в двоичную или восьмеричную!")
-print()
 input("Чтобы продолжить нажмите ENTER")
-NMB = int(input("Введите число: "))
 print()
+NMB = int(input("Введите число: "))
 print("Выберите  2  если, чтоб число переволось в двоичную СС" )
 print("Выберите  8  если хотите, чтоб число перевелось в восьмиричную СС" )
 CHOICE = input("Введите 2 или 8: ")
-def division(N, C, ONE):
+def division(N, C, O):
     NNMB = ""
-    while N != ONE:
-        N = N // int(C)
+    while N > O:
         RMN = N % int(C)
+        N = N // int(C)
         NNMB = str(NNMB) + str(RMN)
     RMN = N % int(C)
-    NNMB = str(RMN) + str(NNMB)
+    NNMB = str(NNMB) + str(RMN)
     N = NNMB
     return N
 while CHOICE != "2" and CHOICE != "8":
     CHOICE = input("Ошибка! Введите 2 или 8: ")
-if CHOICE == "2":
-    NMB = division(NMB, CHOICE, 1)
-else:
-    NMB = division(NMB, CHOICE, 7)
+NMB = division(NMB, CHOICE, int(CHOICE) - 1)
 INV = ""
 for i in range(len(NMB)):
-    INV = NMB[i]+INV
+    INV = NMB[i] + INV
 NMB = INV
-print(NMB)
+print()
+if CHOICE == "2":
+    CHOICE = "двоичной"
+else:
+    CHOICE = "восмиричной"
+print("Ваше число в", CHOICE, "системе счисления:", NMB)
+print()
+input("Чтобы завершить нажмите ENTER")
